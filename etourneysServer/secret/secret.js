0640e4a2e3b5d6275e81b26e8c2a055194cc7f37
Evan <Evanayounan@gmail.com>
2019-04-04 18:15:14 -0400

    secrets

---
+++ etourneysServer/secret/secret.js
@@ -0,0 +1,19 @@
+const secretConfig = {
+    mongoAdminUser: 'admin-provider',
+    mongoAdminPw: 'giustenandevan1957123',
+    clusters: {
+        etournamentCluster: 'etourneys'
+    },
+    port: 3543,
+    riotApiKey: 'RGAPI-5326c8f7-4203-4e4c-9212-fc36c97fde45',
+    firebaseConfig: {
+        apiKey: "AIzaSyD2z0-nTO3G8_M868q9Z1HCOD4YFCj6r_8",
+        authDomain: "chat-app-4c1df.firebaseapp.com",
+        databaseURL: "https://chat-app-4c1df.firebaseio.com",
+        projectId: "chat-app-4c1df",
+        storageBucket: "chat-app-4c1df.appspot.com",
+        messagingSenderId: "962356033029"
+    }
+}
+
+module.exports = secretConfig;
---
+++ src/secret/config.js
@@ -0,0 +1,14 @@
+module.exports = { 
+
+    riotApiKey: 'RGAPI-5326c8f7-4203-4e4c-9212-fc36c97fde45',
+    rootRiotApiLink: 'https://na1.api.riotgames.com',
+    backendRootLink: 'http://localhost:3543',
+    firebaseConfig: {
+        apiKey: "AIzaSyD2z0-nTO3G8_M868q9Z1HCOD4YFCj6r_8",
+        authDomain: "chat-app-4c1df.firebaseapp.com",
+        databaseURL: "https://chat-app-4c1df.firebaseio.com",
+        projectId: "chat-app-4c1df",
+        storageBucket: "chat-app-4c1df.appspot.com",
+        messagingSenderId: "962356033029"
+    }
+}
