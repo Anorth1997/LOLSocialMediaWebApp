3c9c6eadda47545c4a9c9a2651b6104b2a0ce4ad
Ziyao Chen <31010490+Anorth1997@users.noreply.github.com>
2020-02-01 17:47:13 -0500

    Update secret.js

--- etourneysServer/secret/secret.js
+++ etourneysServer/secret/secret.js
@@ -1,18 +1,18 @@
 const secretConfig = {
     mongoAdminUser: 'admin-provider',
-    mongoAdminPw: 'giustenandevan1957123',
+    mongoAdminPw: '',
     clusters: {
         etournamentCluster: 'etourneys'
     },
     port: 3543,
-    riotApiKey: 'RGAPI-1556a5fe-8d53-4782-971b-01df1484ffef',
+    riotApiKey: ,
     firebaseConfig: {
-        apiKey: "AIzaSyD2z0-nTO3G8_M868q9Z1HCOD4YFCj6r_8",
-        authDomain: "chat-app-4c1df.firebaseapp.com",
-        databaseURL: "https://chat-app-4c1df.firebaseio.com",
-        projectId: "chat-app-4c1df",
-        storageBucket: "chat-app-4c1df.appspot.com",
-        messagingSenderId: "962356033029"
+        apiKey: ,
+        authDomain: ,
+        databaseURL: ,
+        projectId: ,
+        storageBucket: ,
+        messagingSenderId: 
     }
 }
 
